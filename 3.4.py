3b211bd724fdd82b6168eab5f8ce85dec1feee00
Sony205 <150188460+Sony205@users.noreply.github.com>
2025-10-02 17:27:11 +0300

    Add 3.4

---
+++ 3.4.py
@@ -0,0 +1,16 @@
+class Countdown:
+    def __init__(self, n):
+        self.n = n
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.n <= 0:
+            raise StopIteration
+        c = self.n
+        self.n -= 1
+        return c
+
+for number in Countdown(5):
+    print(number)
