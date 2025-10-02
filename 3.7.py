3e02f1a1e69de103e7f63a74bf1f51d7133e0a4f
Sony205 <150188460+Sony205@users.noreply.github.com>
2025-10-02 17:43:15 +0300

    Add 3.7

---
+++ 3.7.py
@@ -0,0 +1,15 @@
+from fractions import Fraction
+
+
+frac1 = Fraction(3, 4)
+frac2 = Fraction(5, 6)
+
+addition = frac1 + frac2
+subtraction = frac1 - frac2
+multiplication = frac1 * frac2
+division = frac1 / frac2
+
+print(f"3/4 + 5/6 = {addition}")
+print(f"3/4 - 5/6 = {subtraction}")
+print(f"3/4 * 5/6 = {multiplication}")
+print(f"3/4 / 5/6 = {division}")
